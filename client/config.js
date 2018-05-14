059c2f3e74da66f7842fe2f5e77cec92d755aebc
Fastone <2347632@qq.com>
2018-05-14 22:48:36 +0800

    can login

--- client/config.js
+++ client/config.js
@@ -17,7 +17,7 @@ var config = {
         // 登录地址，用于建立会话
         loginUrl: `${host}/weapp/login`,
         // 测试的请求地址，用于测试会话
-        requestUrl: `${host}/weapp/user`,
+        userUrl: `${host}/weapp/user`,
         // 测试的信道服务地址
         tunnelUrl: `${host}/weapp/tunnel`,
         // 上传图片接口
