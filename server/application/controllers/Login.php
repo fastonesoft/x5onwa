6535a454af6b80594c47f2c6a8d9ba4451802e24
Fastone <2347632@qq.com>
2018-05-21 09:30:06 +0800

    start code

--- server/application/controllers/Login.php
+++ server/application/controllers/Login.php
@@ -10,33 +10,10 @@ class Login extends CI_Controller {
         $result = LoginService::login();
 
         if ($result['loginState'] === Constants::S_AUTH) {
-     // 登录完毕，检测用户是否保存
-            $infor = $result['userinfo'];
-            $uid ='$infor->unionId';
-            $name = '$infor->nickName';
-            $mobil = NULL;
-            $fixed = 0;
-            $is_teacher = 0;
-            $create_time = date('Y-m-d H:i:s');
-            $last_visit_time = date('Y-m-d H:i:s');
-
-            $res = DB::row('xonUser', ['*'], compact('uid'));
-            if ($res === NULL) {
-                DB::insert('xonUser', compact('uid', 'name', 'mobil', 'fixed', 'is_teacher', 'create_time','last_visit_time'));
-            } else {
-                DB::update(
-                    'xonUser',
-                    compact('name', 'mobil', 'fixed', 'is_teacher', 'last_visit_time'),
-                    compact('uid')
-                );
-            }
-
-
             $this->json([
                 'code' => 0,
                 'data' => $result['userinfo']
             ]);
-            return;
         } else {
             $this->json([
                 'code' => -1,
