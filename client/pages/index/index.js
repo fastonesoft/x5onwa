7fbc24c2fd998712575527e8aa1c1ef7f5a23866
Fastone <2347632@qq.com>
2018-06-06 10:21:13 +0800

    now to start

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -7,9 +7,8 @@ Page({
     logged: false,
     cores: [],
     imgUrls: [
-      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
-      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      'http://wafer-1253456186.cosgz.myqcloud.com/f2d1b449ca7d96c3efc1184d37d2caf6-wxdca8673d324d4384.o6zAJs-J33VQGQKJ2oYgoE6vovRY.2DUKCj5D6WKweaa223c31562996d934eec95c8409cc9.jpg',
+      'http://wafer-1253456186.cosgz.myqcloud.com/1fabdbb482d0f6090f8d05ccf9264b94-wxdca8673d324d4384.o6zAJs-J33VQGQKJ2oYgoE6vovRY.X62o9YlGobFN2ed24adb15cdbd96fd24e656395864c2.jpg'
     ]
   },
 
