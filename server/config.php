7b9b5d97801ba6f110f1e76ee174d8105f98df06
fastonesoft <2347632@qq.com>
2018-06-29 11:01:47 +0800

    ready to regstud

--- server/config.php
+++ server/config.php
@@ -25,7 +25,7 @@ $config = [
         'port' => 3306,
         'user' => 'root',
         'db'   => 'cAuth',
-        'pass' => 'stone.2.wx',
+        'pass' => 'wxdca8673d324d4384',
         'char' => 'utf8mb4'
     ],
 
