3c62a6f5cdc127d9292ad37f0779922cc86a340b
qc_577341ddbac24ae885e0b9bde7b6df8f <qc_577341ddbac24ae885e0b9bde7b6df8f@git.code.tencent.com>
2018-11-16 08:17:47 +0800

    start students 6

---
+++ server/application/controllers/Oauth.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use QCloud_WeApp_SDK\Model;
+use \QCloud_WeApp_SDK\Mvv;
+
+class Oauth extends CI_Controller {
+
+  public function index() {
+    $code = $_GET["code"];
+    $appid = "wxecca347b5d64f5c3";
+    $secret = "46c045d9a492fd5108b37d40a1e295be";
+    if (!empty($code))  //有code
+    {
+      //通过code获得 access_token + openid
+      $url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+      $jsonResult = file_get_contents($url);
+      $resultArray = json_decode($jsonResult, true);
+      $access_token = $resultArray["access_token"];
+      $openid = $resultArray["openid"];
+
+      //通过access_token + openid 获得用户所有信息,结果全部存储在$infoArray里,后面再写自己的代码逻辑
+      $infoUrl = "https://api.weixin.qq.com/sns/userinfo?access_token=$access_token&openid=$openid";
+      $infoResult = file_get_contents($infoUrl);
+      $infoArray = json_decode($infoResult, true);
+
+      // 返回信息
+      $this->json(['code' => 0, 'data' => $infoArray]);
+    }
+  }
+
+}
--- server/application/views/welcome_message.php
+++ server/application/views/welcome_message.php
@@ -3,68 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>腾讯云微信小程序服务器 Demo - PHP</title>
-    <style type="text/css">
+  <meta charset="UTF-8">
+  <title>实验初中校务在线应用平台</title>
+  <script src="https://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js"></script>
 
-    ::selection { background-color: #E13300; color: white; }
-    ::-moz-selection { background-color: #E13300; color: white; }
+  <script type="text/javascript">
 
-    body {
-        background-color: #fff;
-        margin: 40px;
-        font: 13px/20px normal Helvetica, Arial, sans-serif;
-        color: #4F5155;
-    }
+    //扫码登录
+    var obj = new WxLogin({
+      id: "login_code",
+      appid: "wxecca347b5d64f5c3",
+      scope: "snsapi_login",
+      redirect_uri: "https://www.x5on.cn/qrcode",
+      state: "web",
+      style: "black",
+      href: "https://www.x5on.cn/content/codesite.css"
+    });
 
-    a {
-        color: #003399;
-        background-color: transparent;
-        font-weight: normal;
-        text-decoration: none;
-    }
+  </script>
 
-    h1 {
-        color: #444;
-        background-color: transparent;
-        border-bottom: 1px solid #D0D0D0;
-        font-size: 19px;
-        font-weight: normal;
-        margin: 0 0 14px 0;
-        padding: 14px 0;
-    }
-
-    p.footer {
-    	text-align: right;
-    	font-size: 11px;
-    	border-top: 1px solid #D0D0D0;
-    	line-height: 32px;
-    	padding: 0 10px 0 10px;
-    	margin: 20px 0 0 0;
-    }
-
-    #container {
-        margin: 10px;
-        padding: 10px 20px;
-        border: 1px solid #D0D0D0;
-        box-shadow: 0 0 8px #D0D0D0;
-    }
-    </style>
 </head>
 <body>
-    <div id="container">
-        <h1>腾讯云微信小程序服务端 Demo - PHP</h1>
-        <p>会话管理服务</p>
-        <ul>
-            <li><a href="/login">登录服务</a></li>
-            <li><a href="/user">检查登录</a></li>
-        </ul>
-        <p>信道服务</p>
-        <ul>
-            <li><a href="/tunnel">获得信道地址</a></li>
-        </ul>
+  <div id="login_code" style="height: 600px; width: 800px;">
 
-        <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-    </div>
+  </div>
 </body>
 </html>
