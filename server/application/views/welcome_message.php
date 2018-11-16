d62415173b1c10eca7784d98b01f5163f2ce1d6f
fastonesoft <2347632@qq.com>
2018-11-16 18:59:36 +0800

    start students

--- server/application/views/welcome_message.php
+++ server/application/views/welcome_message.php
@@ -5,27 +5,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
   <meta charset="UTF-8">
   <title>实验初中校务在线应用平台</title>
+  <script src="https://res.wx.qq.com/connect/zh_CN/htmledition/js/jquery.min3696b4.js"></script>
   <script src="https://res.wx.qq.com/connect/zh_CN/htmledition/js/wxLogin.js"></script>
-
   <script type="text/javascript">
 
-    //扫码登录
-    var obj = new WxLogin({
-      id: "login_code",
-      appid: "wxecca347b5d64f5c3",
-      scope: "snsapi_login",
-      redirect_uri: "https://www.x5on.cn/qrcode",
-      state: "web",
-      style: "black",
-      href: "https://www.x5on.cn/content/codesite.css"
-    });
+      $(function () {
+          //扫码登录
+          var obj = new WxLogin({
+              id: "login_code",
+              appid: "wxecca347b5d64f5c3",
+              scope: "snsapi_login",
+              redirect_uri: "https://www.x5on.cn/oauth",
+              state: "web",
+              style: "black",
+              href: "https://www.x5on.cn/content/codesite.css"
+          });
+      });
 
   </script>
 
 </head>
 <body>
-  <div id="login_code" style="height: 600px; width: 800px;">
+<div id="login_code" style="height: 600px; width: 800px;">
 
-  </div>
+</div>
 </body>
 </html>
