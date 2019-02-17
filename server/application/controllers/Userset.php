366e0cb7aeb045e9cea5c82bf13c59c7c86c0811
qc_577341ddbac24ae885e0b9bde7b6df8f <qc_577341ddbac24ae885e0b9bde7b6df8f@git.code.tencent.com>
2019-02-17 14:56:53 +0800

    start student 2.17

--- server/application/controllers/Userset.php
+++ server/application/controllers/Userset.php
@@ -30,10 +30,12 @@ class Userset extends CI_Controller {
         $mobil = $param['mobil'];
 
         // 变更用户信息
-        Model\xonUser::add($userinfor, $name, $mobil);
+        $user_id = $userinfor->unionId;
+        $nick_name = $userinfor->nickName;
+        $checked = 1;
+        Model\xonUser::setsById(compact('nick_name', 'name', 'mobil', 'checked'), $user_id);
 
         // 返回用户信息
-        $user_id = $userinfor->unionId;
         $result = Model\xonUser::getById($user_id);
 
         $this->json(['code' => 0, 'data' => $result]);
