cbb1f10b7cabc54cf8c8d426d71d135fffcd6028
fastonesoft <2347632@qq.com>
2019-03-28 11:35:29 +0800

    mystud

--- client/pages/index/mystud.js
+++ client/pages/index/mystud.js
@@ -3,34 +3,5 @@ var x5on = require('../x5on.js');
 
 Page({
 
-  data: {
-    dd: [{ name: 'name1' }, { name: 'name2' }]
-  },
-
-  onLoad: function (options) {
-
-  },
-
-  findSubmit: function (e) {
-    var that = this
-    var rules = {
-      name: {
-        required: true,
-        idcard: true,
-      },
-      name1: {
-        required: true,
-        custom: '^\\d{3}-\\d{2}-\\d{2}$'
-      }
-    }
-    var messages = {}
-
-    x5on.checkForm(e, rules, messages, form => {
-      that.setData({ form })
-    }, message => {
-      x5on.showError(that, message)
-    })
-
-  }
 
 })
