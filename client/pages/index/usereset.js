91a2ae68d67688d0d0285e83a797b0fda26849ee
fastonesoft <2347632@qq.com>
2019-06-20 15:00:48 +0800

    usereset

--- client/pages/index/usereset.js
+++ client/pages/index/usereset.js
@@ -5,29 +5,13 @@ Page({
 
   findSubmit: function (e) {
     var that = this
-    var rules = {
-      name: {
-        required: true,
-        chinese: true,
-        rangelength: [1, 3],
-      }
-    }
-    var messages = {
-      name: {
-        required: '用户姓名'
-      }
-    }
-    x5on.checkForm(e.detail.value, rules, messages, form => {
-      x5on.post({
-        url: x5on.url.usereset,
-        data: form,
-        success(users) {
-          that.setData({ users, user: null })
-          users.length === 0 && x5on.showError('没有找到你要的用户！')
-        }
-      })
-    }, message => {
-      x5on.showError(message)
+    x5on.http(x5on.url.usereset, e.detail)
+    .then(users=>{
+      users.length !== 0 && that.setData({ users, user: null })
+      users.length === 0 && x5on.showError('没有找到你要的用户！')
+    })
+    .catch(error=>{
+      x5on.showError(error)
     })
   },
 
