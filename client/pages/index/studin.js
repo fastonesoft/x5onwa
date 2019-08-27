59ab30f6f87e5a8181966c9b249242c888a2584d
fastonesoft <2347632@qq.com>
2019-08-27 11:57:38 +0800

    change

--- client/pages/index/studin.js
+++ client/pages/index/studin.js
@@ -5,24 +5,6 @@ Page({
 
 	onLoad: function (e) {
 		var that = this
-		var fields = [{
-			mode: 1,
-			label: '分组人数',
-			name: 'group_num',
-			type: 'number',
-			message: '请输入分组人数',
-			value: 36,
-			maxlength: 2,
-		  }]
-		  var rules = {
-			group_num: {
-			  required: true,
-			  digits: true,
-			  maxlength: 2,
-			},
-		  }
-		that.setData({ fields, rules })
-
 		x5on.http(x5on.url.studin)
 			.then(steps => {
 				that.setData({ steps })
@@ -37,7 +19,7 @@ Page({
 		that.setData(e.detail)
 		x5on.http(x5on.url.studinnotin, e.detail)
 		.then(notins => {
-			that.setData({ notins })
+			that.setData({ notins, ins: [] })
 		})
 		.catch(error => {
 			x5on.showError(error)
